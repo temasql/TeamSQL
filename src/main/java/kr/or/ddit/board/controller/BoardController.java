49f1627336a7863e16d9bc1554b6293068b77f7e
shookie <shookiejovial@gmail.com>
2019-07-19 17:56:50 +0900

    게시판 관리
      - 신규 게시판 생성, 기존 게시판 사용여부 변경 기능 작업(dao, service, controller)

--- src/main/java/kr/or/ddit/board/controller/BoardController.java
+++ src/main/java/kr/or/ddit/board/controller/BoardController.java
@@ -1,20 +1,93 @@
 package kr.or.ddit.board.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//@RequestMapping("/boardController")
-//@Controller
-public class BoardController {
+import kr.or.ddit.board.model.BoardVO;
+import kr.or.ddit.board.service.IBoardService;
+import kr.or.ddit.user.model.UserVO;
 
-	@RequestMapping(path =  "/view", method = RequestMethod.GET)
+@RequestMapping("/board")
+@Controller
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Resource(name="boardService")
+	private IBoardService boardService;
+	
+	
+	/**
+	* Method : viewGet
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @return
+	* Method 설명 : 게시판 관리 메인 화면 요청
+	*/
+	@RequestMapping("/manager")
 	public String viewGet() {
-		return "";
+		return "/admin/boardMG.tiles";
 	}
 	
-	@RequestMapping(path =  "/view", method = RequestMethod.POST)
-	public String viewPost() {
-		return "";
+	
+	/**
+	* Method : addBoard
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @param session
+	* @param board_name
+	* @param board_use
+	* @return
+	* Method 설명 : 게시판 추가
+	*/
+	@RequestMapping(path =  "/addBoard", method = RequestMethod.POST)
+	public String addBoard(HttpSession session, String board_name, String board_use) {
+		String user_id = ((UserVO) session.getAttribute("USER_INFO")).getUser_id();
+		
+		BoardVO boardVo = new BoardVO(user_id, board_name, board_use);
+		
+		if(boardService.addBoard(boardVo) == 1 ) {
+			session.setAttribute("boardList", boardService.boardList());
+			return "/admin/boardMG.tiles";
+		} else {
+			return "/admin/boardMG.tiles";
+		}
 	}
+	
+	
+	/**
+	* Method : modifyBoard
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @param session
+	* @param board_id
+	* @param updateBoardName
+	* @param updateUse_yn
+	* @return
+	* Method 설명 : 게시판 수정
+	*/
+	@RequestMapping(path = "/modifyBoard", method = RequestMethod.POST)
+	public String modifyBoard(HttpSession session, int board_id, String updateBoardName, String updateBoard_use) {
+		String user_id = ((UserVO) session.getAttribute("USER_INFO")).getUser_id();
+		
+		BoardVO boardVo = new BoardVO(board_id, user_id, updateBoardName, updateBoard_use);
+		
+		int updateCnt = boardService.modifyBoard(boardVo);
+		
+		if(updateCnt == 1) {
+			List<BoardVO> boardList = boardService.boardList();
+			session.setAttribute("boardList", boardList);
+		}
+		return "/admin/boardMG.tiles";
+	}
+
+	
 }
--- src/main/java/kr/or/ddit/board/service/BoardService.java
+++ src/main/java/kr/or/ddit/board/service/BoardService.java
@@ -6,45 +6,83 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
 import kr.or.ddit.board.dao.IBoardDao;
 import kr.or.ddit.board.model.BoardVO;
 
-//@Service
+@Service
 public class BoardService implements IBoardService{
 
 	@Resource(name = "boardDao")
 	private IBoardDao boardDao;
 
-	@Override
-	public int insert(BoardVO boardVo) {
-		return boardDao.insert(boardVo);
-	}
 	
+	/**
+	* Method : boardList
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @return
+	* Method 설명 : 게시판 리스트 조회
+	*/
 	@Override
-	public BoardVO get(String id) {
-		return boardDao.get(id);
-	}
-	
-	@Override
-	public List<BoardVO> list() {
-		return boardDao.list();
+	public List<BoardVO> boardList() {
+		return boardDao.boardList();
 	}
 
+	
+	/**
+	* Method : boardMaxCnt
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @return
+	* Method 설명 : 테스트용 게시판 아이디 생성
+	*/
 	@Override
-	public Map<String, Object> map(Map<String, Object> map) {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		List<BoardVO> mapList =  boardDao.map(map);
-		return resultMap;
+	public int boardMaxCnt() {
+		return boardDao.boardMaxCnt();
 	}
 
-	@Override
-	public int update(String id) {
-		return boardDao.update(id);
-	}
 	
+	/**
+	* Method : addBoard
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @param boardVo
+	* @return
+	* Method 설명 : 신규 게시판 생성
+	*/
 	@Override
-	public int delete(String id) {
-		return boardDao.delete(id);
+	public int addBoard(BoardVO boardVo) {
+		return boardDao.addBoard(boardVo);
+	}
+
+	
+	/**
+	* Method : modifyBoard
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @param boardVo
+	* @return
+	* Method 설명 : 게시판 사용여부 변경
+	*/
+	@Override
+	public int modifyBoard(BoardVO boardVo) {
+		return boardDao.modifyBoard(boardVo);
+	}
+
+	
+	/**
+	* Method : getBoard
+	* 작성자 : 이영은
+	* 변경이력 :
+	* @param board_id
+	* @return
+	* Method 설명 : 입력받은 게시판 아이디와 일치하는 게시판의 정보
+	*/
+	@Override
+	public BoardVO getBoard(int board_id) {
+		return boardDao.getBoard(board_id);
 	}
 
 }
