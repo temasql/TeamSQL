9fa2aacc746e899ba9cdbeb0c55d613b8904673a
shookie <shookiejovial@gmail.com>
2019-07-19 17:58:22 +0900

    게시판 관리
     - 게시판 관리 섹션 테스트
        : BoardDaoTest, BoardServiceTest, BoardControllerTest

--- src/test/java/kr/or/ddit/board/service/BoardServiceTest.java
+++ src/test/java/kr/or/ddit/board/service/BoardServiceTest.java
@@ -2,10 +2,13 @@ package kr.or.ddit.board.service;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 
+import kr.or.ddit.board.model.BoardVO;
 import kr.or.ddit.testenv.LogicTestEnv;
 
 /**
@@ -26,17 +29,84 @@ import kr.or.ddit.testenv.LogicTestEnv;
 */
 public class BoardServiceTest extends LogicTestEnv{
 
-//	@Resource(name = "서비스")
-//	private ICommonsService service;
+	@Resource(name = "boardService")
+	private IBoardService service;
 	
+	
+	/**
+	* Method : boardListTest
+	* 작성자 : 이영은
+	* 변경이력 :
+	* Method 설명 : 게시판 리스트 조회 테스트
+	*/
 	@Test
-	public void serviceTest() {
+	public void boardListTest() {
 		/***Given***/
-
+		
 		/***When***/
+		List<BoardVO> boardList = service.boardList();
 		
 		/***Then***/
-		assertEquals("", "");
+		assertEquals(3, boardList.size());
 	}
+	
+	
+	/**
+	* Method : addBoardTest
+	* 작성자 : 이영은
+	* 변경이력 :
+	* Method 설명 : 신규 게시판 생성 테스트
+	*/
+	@Test
+	public void addBoardTest() {
+		/***Given***/
+		int testId = service.boardMaxCnt();
+		BoardVO boardVo = new BoardVO(testId, "admin", "Q&A", "y");
 
+		/***When***/
+		int addCnt = service.addBoard(boardVo);
+		
+		/***Then***/
+		assertEquals(1, addCnt);
+	}
+	
+	
+	/**
+	* Method : modifyBoardTest
+	* 작성자 : 이영은
+	* 변경이력 :
+	* Method 설명 : 게시판 사용여부 변경 테스트
+	*/
+	@Test
+	public void modifyBoardTest() {
+		/***Given***/
+		int testId = service.boardMaxCnt();
+		BoardVO boardVo = new BoardVO(testId-1, "admin", "테스트게시판3", "N");
+
+		/***When***/
+		int updateCnt = service.modifyBoard(boardVo);
+		
+		/***Then***/
+		assertEquals(1, updateCnt);
+	}
+	
+	
+	/**
+	* Method : getBoardTest
+	* 작성자 : 이영은
+	* 변경이력 :
+	* Method 설명 : 입력받은 게시판 아이디와 일치하는 게시판의 정보 테스트
+	*/
+	@Test
+	public void getBoardTest() {
+		/***Given***/
+		int testId = service.boardMaxCnt();
+		int board_id = testId-1;
+
+		/***When***/
+		BoardVO boardVo = service.getBoard(board_id);
+		
+		/***Then***/
+		assertEquals("테스트게시판3", boardVo.getBoard_name());
+	}
 }
