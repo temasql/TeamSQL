9e8676145259574decfe983542738693c5812625
29ena <zigedge@gmail.com>
2019-07-22 17:49:27 +0900

    챗봇 구현중

--- src/main/webapp/resources/chatBot/js/chatBotJS.js
+++ src/main/webapp/resources/chatBot/js/chatBotJS.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
+	$("#questionBtn").on('click',function(){
+		var question= $("#userInput").val();
+		alert(question);
+		$(".userLi").append("<span>유저</span><div class='userQuestion'>"+question+"</div>").val();
+		$("#userInput").val("");
+	});
+	
+	var html = "";
 	
 	
+	
+//	   <li class="userLi">
+//	    <span>유저</span>
+//	    <div class="userQuestion">
+//	    	<p class="userMSG" id="idUserMSG">테이블 생성하는 방법</p>
+//	     </div>
+//	   </li>
 	
 });
