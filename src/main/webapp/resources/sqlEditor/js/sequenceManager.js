6a6a15d01bb78119292f79ab2e91b452f1701818
ena <zigedge@gmail.com>
2019-08-01 17:23:25 +0900

    시퀀스 생성 구현중

--- src/main/webapp/resources/sqlEditor/js/sequenceManager.js
+++ src/main/webapp/resources/sqlEditor/js/sequenceManager.js
@@ -4,10 +4,17 @@ $(document).ready(function() {
 	$("#createsequenceSpan").on("click", function() {
 		$("#craeteSequenceModal").css("display", "block");
 	});
+	
+	// 
+	
+	// 시퀀스 생성
+	$(document).on("click","#createSeqBtn",function(){
+		var seqName = $("#sequenceName").val();
+		var seqReg = /^[a-zA-Z][a-zA-Z0-9#$_]{2,8}$/;
+	})
 
 	// 모달창 닫기
 	$(".close").on("click", function() {
 		$("#craeteSequenceModal").css("display", "none");
 	});
-
 });
