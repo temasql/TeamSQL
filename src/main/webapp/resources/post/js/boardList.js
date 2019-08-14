c3f1622a9129fe32f88187a9f50f8a1842b4d83d
shookie <shookiejovial@gmail.com>
2019-08-14 15:51:46 +0900

    게시판 / 공지사항 기능
    - 게시판 검색 기능 추가 및 화면 수정
    - 공지사항 기능 추가
    
    * 게시판 기능 완료

--- src/main/webapp/resources/post/js/boardList.js
+++ src/main/webapp/resources/post/js/boardList.js
@@ -12,14 +12,20 @@ $(function(){
 			$("#frm").submit();
 		}
 	})
+	
+	$("#btnSearch").on("click", function(){
+		postPagingListAjaxHtml(1,10);
+	})
 })
 
 function postPagingListAjaxHtml(page, pageSize) {
 	var board_id = $("#board_id").val()
+	var search = $("#searchfor").val()
+	var select = $("#select").val()
 	$.ajax({
 		  url : "/post/boardList"
 		, method : "post"
-		, data : "page=" + page + "&pageSize=" + pageSize + "&board_id="  + board_id 
+		, data : "page=" + page + "&pageSize=" + pageSize + "&board_id="  + board_id + "&search=" + search + "&selectBox=" + select 
 		, success : function(data) {
 			console.log(data)
 			var html = data.split("SEPERATORSEPERATOR");
