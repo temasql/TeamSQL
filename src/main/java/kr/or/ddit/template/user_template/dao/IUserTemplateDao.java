4b53f2277ce19b6435a4a7fe7a5de35942b485e6
juhyung <wthswngud1@naver.com>
2019-08-15 13:53:40 +0900

    템플릿 수정 안되는 현상 수정

--- src/main/java/kr/or/ddit/template/user_template/dao/IUserTemplateDao.java
+++ src/main/java/kr/or/ddit/template/user_template/dao/IUserTemplateDao.java
@@ -22,4 +22,14 @@ public interface IUserTemplateDao {
 	int updateUserTemplate(UserTemplateVO userTemplateVO);
 
 	int deleteUserTemplate(UserTemplateVO userTemplateVO);
+	
+	/**
+	* Method : sameAbb
+	* 작성자 : 손주형
+	* 변경이력 :
+	* @param map
+	* @return
+	* Method 설명 : 입력한 템플릿ID와 일치하는 약어 검색
+	*/
+	String sameAbb(Map<String, Object> map);
 }
